c58fe57db482551e56973d9e0a520eb642c4b1f5
Aymen Jdily <87182808+Aymenjdily@users.noreply.github.com>
2023-01-26 17:28:19 +0100

    first commit

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+    define('DB_HOST','localhost');
+    define('DB_USER','aymen');
+    define('DB_PASS','19960807');
+    define('DB_NAME','feedbacks');
+    
+    $connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+    if($connection -> connect_error){
+        die('Connection Failed' . $connection -> connect_error);
+    }
+
+?>
